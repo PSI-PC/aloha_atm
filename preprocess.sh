f0943972acb21b622433b2e47a90d286a67257c3
student_pool <pascal.siekiera@gmail.com>
2025-02-17 16:23:02 +0100

    added preprocess script for unicluster

---
+++ preprocess.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --ntasks=40
+#SBATCH --time=08:00:00
+#SBATCH --mem=64000
+#SBATCH --gres=gpu:1
+
+conda env create -f environment.yml
+conda activate aloha_atm
+
+python3 scripts/preprocess_aloha.py
