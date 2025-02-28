857e852a5a6498806d1114b357fdc8d5f51f5ebe
student_pool <pascal.siekiera@gmail.com>
2025-02-28 16:36:10 +0100

    update config

--- train_transformers.sh
+++ train_transformers.sh
@@ -9,6 +9,9 @@ module load devel/miniconda
 conda env create -f environment.yml
 conda activate aloha_atm
 
+pip install -e third_party/robosuite/
+pip install -e third_party/robomimic/
+
 export WANDB_API_KEY="c44cc0218f3c141d46861ac42fe23e25ff6745a4"
 
 python3 scripts/split_aloha_dataset.py
